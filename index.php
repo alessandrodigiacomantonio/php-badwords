3eca01bc53637459fe01a32dc0fbb5532d83c0e4
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-02-20 18:04:10 +0100

    aggiunto $_GET, fixato &bad_worLd :D

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   $txt_content = "Le stazioni si somigliano tutte; poco importa se le luci non riescono a rischiarare più in là del loro alone sbavato, tanto questo è un ambiente che tu conosci a memoria, con l'odore di treno che resta anche dopo che tutti i treni sono partiti, l'odore speciale delle stazioni dopo che è partito l'ultimo treno. Le luci della stazione e le frasi che stai leggendo sembra abbiano il compito di dissolvere più che di indicare le cose affioranti da un velo di buio e di nebbia. Io sono sbarcato in questa stazione stasera per la prima volta in vita mia e già mi sembra d'averci passato una vita, entrando e uscendo da questo bar, passando dall'odore della pensilina all'odore di segatura bagnata dei gabinetti, tutto mescolato in un unico odore che è quello dell'attesa, l'odore delle cabine telefoniche quando non resta che recuperare i gettoni perché il numero chiamato non dà segno di vita.";
-  $bad_world = "memoria";
+  $bad_word = $_GET["bad-word"];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,7 +23,7 @@
   </p>
   <p>
     <?php
-      echo $txt_content_filtered = str_replace($bad_world, '+++', $txt_content);
+      echo $txt_content_filtered = str_replace($bad_word, '+++', $txt_content);
     ?>
   </p>
   <p>
